d4de15c1032b45423d2da57ec2fc3f5d840ad751
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-03-30 12:59:21 +0530

    AuthController.java

--- carportal/src/main/java/com/carportal/controller/AuthController.java
+++ carportal/src/main/java/com/carportal/controller/AuthController.java
@@ -23,7 +23,7 @@ public class AuthController {
         this.userRepository = userRepository;
         this.authService = authService;
     }
-   @PostMapping
+   @PostMapping("/user/sign-up")
     public ResponseEntity<String> createUser(
             @RequestBody User user) {
        Optional<User> opEmail = userRepository.findByEmail(user.getEmail());
@@ -42,19 +42,44 @@ public class AuthController {
        }
        String hashpw = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(12));
        user.setPassword(hashpw);
+       user.setRole("ROLE_USER");
        userRepository.save(user);
        return new ResponseEntity<>("User created successfully", HttpStatus.CREATED);
    }
+
+    @PostMapping("/owner/sign-up")
+    public ResponseEntity<String> createOwner(
+            @RequestBody User user) {
+        Optional<User> opEmail = userRepository.findByEmail(user.getEmail());
+        if (opEmail.isPresent()) {
+            return new ResponseEntity<>("Email id exists", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        Optional<User> opMobile = userRepository.findByMobile(user.getMobile());
+        if (opMobile.isPresent()) {
+            return new ResponseEntity<>("Mobile number exists", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        Optional<User> opUsername = userRepository.findByUsername(user.getUsername());
+        if (opUsername.isPresent()) {
+            return new ResponseEntity<>("Username already exists", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        String hashpw = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(12));
+        user.setPassword(hashpw);
+        user.setRole("ROLE_OWNER");
+        userRepository.save(user);
+        return new ResponseEntity<>("User created successfully", HttpStatus.CREATED);
+    }
     @PostMapping("/login")
     public ResponseEntity<String> verifyLogin(
             @RequestBody LoginDto dto
     ){
-        boolean status = authService.authenticate(dto);
-        if(status){
+        String jwtToken = authService.authenticate(dto);
+        if(jwtToken != null){
 
-            return new ResponseEntity<>("verified", HttpStatus.OK);
+            return new ResponseEntity<>(jwtToken, HttpStatus.OK);
         }else {
             return new ResponseEntity<>("Invalid ", HttpStatus.OK);
         }
     }
+
 }
