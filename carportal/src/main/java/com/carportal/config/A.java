16fdc8c04c6a7a044797b6b17e35919a01f7d001
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-03-30 12:57:00 +0530

    A.java

--- carportal/src/main/java/com/carportal/config/A.java
+++ carportal/src/main/java/com/carportal/config/A.java
@@ -4,14 +4,31 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.security.SecureRandom;
+import java.util.Base64;
+
 //Encryption
 public class A {
-    public static void main(String[] args) {
+   // public static void main(String[] args) {
 //        PasswordEncoder encrypt = new BCryptPasswordEncoder();
 //        System.out.println(encrypt.encode("testing"));
 
-        String password = BCrypt.hashpw("testing", BCrypt.gensalt(12));//log_rounds =3, 2^3=8 times password will be encrypted
-        System.out.println(password);
-    }
+ //       String password = BCrypt.hashpw("testing", BCrypt.gensalt(12));//log_rounds =3, 2^3=8 times password will be encrypted
+  //      System.out.println(password);
+   // }
+
+
+//        public static void main(String[] args) {
+//            SecureRandom secureRandom = new SecureRandom();
+//            byte[] key = new byte[32]; // 256 bits = 32 bytes
+//            secureRandom.nextBytes(key);
+//            String secretKey = Base64.getEncoder().encodeToString(key);
+//
+//            System.out.println("Generated Secret Key: " + secretKey);
+//    }
+public static void main(String[] args) {
+    System.out.println(new BCryptPasswordEncoder().encode("testing"));
+}
+
 
 }
